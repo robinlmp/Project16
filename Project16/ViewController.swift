d113f25081cf197d2f3289433e899c300608146a
Robin Phillips <robinlmp@mac.com>
2022-02-19 16:14:53 +0100

    Challenge 2 complete

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Capital cities"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit , target: self, action: #selector(mapSettings))
+        
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
@@ -21,6 +26,26 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+    }
+    
+    @objc func mapSettings() {
+        print("map settings tapped")
+        
+        let ac = UIAlertController(title: "Choose the map style", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "Standard", style: .default) {_ in
+            self.mapView.mapType = .standard
+        })
+        ac.addAction(UIAlertAction(title: "Satellite", style: .default) {_ in
+            self.mapView.mapType = .satellite
+        })
+        ac.addAction(UIAlertAction(title: "Hybrid", style: .default) {_ in
+            self.mapView.mapType = .hybrid
+        })
+        ac.addAction(UIAlertAction(title: "Flyover", style: .default) {_ in
+            self.mapView.mapType = .satelliteFlyover
+        })
+        present(ac, animated: true)
     }
     
     
